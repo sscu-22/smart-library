7ed22e7d07557659be703b3282d2cbd5de3815db
sscu-22 <shwesin.ucsm@gmail.com>
2026-08-30 13:30:28 +0630

    fix allowed hosts

--- digital_library/settings.py
+++ digital_library/settings.py
@@ -112,8 +112,9 @@ SECRET_KEY = "django-insecure-2ho2nbj8(rutseedgaalm41n60q=p^avtke(zsmvg*l5(e_lr#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['smartlibrary.ucsm.edu.mm']
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['
+# ']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
